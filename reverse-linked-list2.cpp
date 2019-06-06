06ad24c90f126b28cfb5cdf44b38e30d27b990ed
Yash Mittal <40431640+MrMittalY@users.noreply.github.com>
2019-06-06 22:16:28 +0530

    Add files via upload

---
+++ reverse-linked-list2.cpp
@@ -0,0 +1,34 @@
+// 206. Reverse Linked List
+// Reverse a singly linked list.
+
+// Example:
+
+// Input: 1->2->3->4->5->NULL
+// Output: 5->4->3->2->1->NULL
+// Follow up:
+
+// A linked list can be reversed either iteratively or recursively. Could you implement both?
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev=NULL;
+        ListNode* nex=head;
+        while(head)
+        {   nex=head->next;
+            head->next=prev;
+            prev=head;
+            head=nex;
+        }      
+        return prev;
+        
+    }
+};

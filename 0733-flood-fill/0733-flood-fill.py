be2345e9c3590c340a2c58dffaeeb428b923512b
Yash Mittal <40431640+yashmittal-git@users.noreply.github.com>
2023-09-01 23:56:52 +0530

    Time: 69 ms (93.54%), Space: 16.4 MB (71.99%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,15 +1,18 @@
 class Solution:
-    def fill4D(self, image: List[List[int]], sr: int, sc: int, color: int, startColor: int) -> List[List[int]]:
-        if sr < 0 or sc < 0 or sr == len(image) or sc == len(image[0]) or image[sr][sc] == color or image[sr][sc] != startColor:
+
+    startColor: int
+
+    def fill4D(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        if sr < 0 or sc < 0 or sr == len(image) or sc == len(image[0]) or image[sr][sc] == color or image[sr][sc] != self.startColor:
             return image
         
         image[sr][sc] = color
-        image = self.fill4D(image,sr+1,sc,color,startColor)
-        image = self.fill4D(image,sr,sc+1,color,startColor)
-        image = self.fill4D(image,sr-1,sc,color,startColor)
-        image = self.fill4D(image,sr,sc-1,color,startColor)
+        image = self.fill4D(image,sr+1,sc,color)
+        image = self.fill4D(image,sr,sc+1,color)
+        image = self.fill4D(image,sr-1,sc,color)
+        image = self.fill4D(image,sr,sc-1,color)
         return image
 
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        return self.fill4D(image,sr,sc,color,image[sr][sc])
-        
+        self.startColor = image[sr][sc]
+        return self.fill4D(image,sr,sc,color)

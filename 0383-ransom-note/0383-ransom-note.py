9d731c18c424bcecbb0e888c5d9bc383f4b362c9
Yash Mittal <40431640+yashmittal-git@users.noreply.github.com>
2026-01-25 22:45:40 +0530

    Time: 25 ms (33.83%), Space: 19.5 MB (32.46%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,12 +1,15 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        
         if len(ransomNote) > len(magazine):
             return False
-
-        for char in ransomNote:
-            if char not in magazine:
-                return False
-            magazine = magazine.replace(char,"",1)
         
-        return True
+        charcount = {}
+        for char in magazine:
+            charcount[char] = charcount.get(char,0) + 1
+        for char in ransomNote:
+            charcount[char] = charcount.get(char,0) - 1
+            if charcount[char] < 0:
+                return False
+        
+
+        return True        

441de10318a1eaa502cc389126353f35f6e079ef
Yash Mittal <40431640+yashmittal-git@users.noreply.github.com>
2026-03-02 01:38:01 +0530

    Time: 89 ms (5.15%), Space: 38.1 MB (6.58%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -2,6 +2,7 @@ class TrieNode:
     def __init__(self, is_end=False):
         self.children = [None for _ in range(26)]
         self.is_end = is_end
+ORD_A = 97
 
 class Trie:
 
@@ -12,32 +13,28 @@ class Trie:
         node = self.node
         for i in range(len(word)):
             char = word[i]
-            if not node.children[ord(char)-ord('a')]:
-                node.children[ord(char)-ord('a')] = TrieNode()
-            node = node.children[ord(char)-ord('a')]
+            if not node.children[ord(char)-ORD_A]:
+                node.children[ord(char)-ORD_A] = TrieNode()
+            node = node.children[ord(char)-ORD_A]
         node.is_end = True
 
     def search(self, word: str) -> bool:
         node = self.node
         for i in range(len(word)):
             char = word[i]
-            if node.children[ord(char)-ord('a')]:
-                node = node.children[ord(char)-ord('a')]
-            else:
+            if not node.children[ord(char)-ORD_A]:
                 return False
-        if node.is_end != True:
-            return False        
-        return True
+            node = node.children[ord(char)-ORD_A]
+        return node.is_end
         
 
     def startsWith(self, prefix: str) -> bool:
         node = self.node
         for i in range(len(prefix)):
             char = prefix[i]
-            if node.children[ord(char)-ord('a')]:
-                node = node.children[ord(char)-ord('a')]
-            else:
+            if not node.children[ord(char)-ORD_A]:
                 return False
+            node = node.children[ord(char)-ORD_A]
         return True
 
 

d349003631a0af695ef061bdf18723fdb0a63727
Yash Mittal <40431640+yashmittal-git@users.noreply.github.com>
2026-03-07 18:48:14 +0530

    Time: 246 ms (54.11%), Space: 21.3 MB (98.73%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,33 +1,26 @@
 class Solution:
-
-    paths = [(-1,0),(1,0),(0,1),(0,-1)]
-    m = 0
     n = 0
+    m = 0
 
-    def bfs(self, grid, i, j):
-        q = [(i,j)]
+    paths = [(-1,0),(1,0),(0,-1),(0,1)]
 
-        while len(q):
-            i1, j1 = q.pop()
-            
-            for path in self.paths:
-                i2 = i1 + path[0]
-                j2 = j1 + path[1]
+    def dfs(self, grid, i, j):
+        if i < 0 or i >= self.n or j < 0 or j >=self.m or grid[i][j]=="0":
+            return
+        grid[i][j] = "0"
+        for path in self.paths:
+            self.dfs(grid,i+path[0],j+path[1])
 
-                if i2 >= 0 and i2 < self.m and j2 >=0 and j2 < self.n and grid[i2][j2] == "1":
-                    grid[i2][j2] = "0"
-                    q.append((i2,j2))
 
     def numIslands(self, grid: List[List[str]]) -> int:
-        self.m = len(grid)
-        self.n = len(grid[0])
+        self.n = len(grid)
+        self.m = len(grid[0])
         answer = 0
 
-        for i in range(self.m):
-            for j in range(self.n):
-                if grid[i][j] == "1":
+        for i in range(self.n):
+            for j in range(self.m):
+                if grid[i][j]=="1":
                     answer = answer + 1
-                    grid[i][j] = "0"
-                    self.bfs(grid, i, j)
+                    self.dfs(grid,i,j)
         
         return answer

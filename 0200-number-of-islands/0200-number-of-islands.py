6d43a99ebe1ae2a3d7952ad2b39c878a0b48ae53
Yash Mittal <40431640+yashmittal-git@users.noreply.github.com>
2026-03-07 18:17:14 +0530

    Time: 252 ms (41.84%), Space: 21.4 MB (98.48%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -0,0 +1,30 @@
+class Solution:
+    n = 0
+    m = 0
+    visited = []
+
+    paths = [(-1,0),(1,0),(0,-1),(0,1)]
+
+    def dfs(self, grid, i, j):
+        if i < 0 or i >= self.n or j < 0 or j >=self.m or grid[i][j]=="0" or self.visited[i][j] == True:
+            return
+        self.visited[i][j] = True
+        grid[i][j] = 0
+        for path in self.paths:
+            self.dfs(grid,i+path[0],j+path[1])
+
+
+    def numIslands(self, grid: List[List[str]]) -> int:
+        self.n = len(grid)
+        self.m = len(grid[0])
+        answer = 0
+
+        self.visited = [[False for _ in range(self.m)] for _ in range(self.n)]
+
+        for i in range(self.n):
+            for j in range(self.m):
+                if grid[i][j]=="1":
+                    answer = answer + 1
+                    self.dfs(grid,i,j)
+        
+        return answer

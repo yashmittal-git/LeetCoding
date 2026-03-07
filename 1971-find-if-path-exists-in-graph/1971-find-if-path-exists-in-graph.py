5ef11e6072a1ef395f2e5895c469e53e4296990c
Yash Mittal <40431640+yashmittal-git@users.noreply.github.com>
2026-03-08 02:04:47 +0530

    Time: 251 ms (84.22%), Space: 83 MB (99.17%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,31 +1,33 @@
+class UnionFind:
+    def __init__(self, n):
+        self.n = n
+        self.rank = [0 for _ in range(n)]
+        self.parent = [i for i in range(n)]
+        
+    def find(self, node):
+        if self.parent[node] == node:
+            return node
+        self.parent[node] = self.find(self.parent[node])
+        return self.parent[node]        
+    
+    def union(self, node1, node2):
+        node1_parent = self.find(node1)
+        node2_parent = self.find(node2)
+        if node1_parent != node2_parent:
+            if self.rank[node1_parent] > self.rank[node2_parent]:
+                self.parent[node2_parent] = node1_parent
+                self.rank[node1_parent] = self.rank[node1_parent] + 1
+            else:
+                self.parent[node1_parent] = node2_parent
+                self.rank[node2_parent] = self.rank[node2_parent] + 1
+
+
 class Solution:
 
-    def bfs(self, adj, visited, source, destination):
-        q = [source]
-        visited[source] = True
-
-        while len(q):
-            node = q.pop()
-
-            for child in adj[node]:
-                if not visited[child]:
-                    if child == destination:
-                        return True
-                    visited[child] = True
-                    q.append(child)
-        
-        return False
-
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        if source == destination:
-            return True
-
-        adj = defaultdict(list)
-
+        uf = UnionFind(n)
         for edge in edges:
-            adj[edge[0]].append(edge[1])
-            adj[edge[1]].append(edge[0])
+            uf.union(edge[0], edge[1])
         
-        visited = [False for _ in range(n)]
-
-        return self.bfs(adj, visited, source, destination)
+        return uf.find(source) == uf.find(destination)
+        

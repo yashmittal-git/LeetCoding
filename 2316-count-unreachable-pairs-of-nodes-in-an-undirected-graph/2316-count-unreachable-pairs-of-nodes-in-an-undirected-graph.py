55350d0effc6a4e58c27edf6fa075b48783f548f
Yash Mittal <40431640+yashmittal-git@users.noreply.github.com>
2026-03-15 00:24:26 +0530

    Time: 200 ms (38.86%), Space: 65.1 MB (75.99%) - LeetHub

--- 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.py
+++ 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.py
@@ -1,34 +1,44 @@
+class UnionFind:
+    def __init__(self, size):
+        self.size = size
+        self.parent = [_ for _ in range(size)]
+        self.rank = [0 for _ in range(size)]
+    
+    def find(self, node):
+        if self.parent[node] == node:
+            return node
+        self.parent[node] = self.find(self.parent[node])
+        return self.parent[node]
+    
+    def union(self, node1, node2):
+        node1_parent = self.find(node1)
+        node2_parent = self.find(node2)
+        if node1_parent == node2_parent:
+            return
+        
+        if self.rank[node1_parent] > self.rank[node2_parent]:
+            self.parent[node2_parent] = node1_parent
+            self.rank[node1_parent] = self.rank[node1_parent] + 1
+        else:
+            self.parent[node1_parent] = node2_parent
+            self.rank[node2_parent] = self.rank[node2_parent] + 1
+
 class Solution:
     def countPairs(self, n: int, edges: List[List[int]]) -> int:
-
-        adjList = defaultdict(list)
-
+        uf = UnionFind(n)
+        components = defaultdict(int)
         for edge in edges:
-            adjList[edge[0]].append(edge[1])
-            adjList[edge[1]].append(edge[0])
+            uf.union(edge[0],edge[1])
         
-        visited = set()
-
-        def bfs(node):
-            count = 0
-            q = [node]
-            visited.add(node)
-            while q:
-                node = q.pop()
-                count = count + 1
-
-                for child in adjList[node]:
-                    if child not in visited:
-                        visited.add(child)
-                        q.append(child)
-            
-            return count
-
+        for node in range(n):
+            parent = uf.find(node)
+            components[parent] = components[parent] + 1
+        
         pairs = 0
-        for i in range(n):
-            if i not in visited:
-                count = bfs(i)
-                n = n - count
-                pairs = pairs + n*count
+        for component, size in components.items():
+            n = n - size
+            pairs = pairs + size*n
         
         return pairs
+        
+        
